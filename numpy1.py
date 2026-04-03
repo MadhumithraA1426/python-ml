a31bbdc5356ca6c4d374373abb459ab8b46722d2
Madhu Mithra <madhumithraoffcl@gmail.com>
2026-04-03 14:35:31 +0530

    Numpy library

---
+++ numpy1.py
@@ -0,0 +1,16 @@
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(arr)
+print(arr+2)
+print(type(arr))
+print(arr.mean())
+print(arr.sum())
+print(arr.min())
+
+""" output
+[1 2 3 4 5]
+[3 4 5 6 7]
+<class 'numpy.ndarray'>
+3.0
+15
+1 """
